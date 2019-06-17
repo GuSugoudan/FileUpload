8ad746a52e31f44d8f6c3d7c79fabdb7e8a5f678
GuSugoudan <35639361+GuSugoudan@users.noreply.github.com>
2019-06-17 20:07:08 +0800

    Create UUIDUtil.java

---
+++ src/com/donann/controller/utils/UUIDUtil.java
@@ -0,0 +1,11 @@
+package com.donann.utils;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+	
+	public static String getuuid() {
+		UUID uuid=UUID.randomUUID();
+		return uuid.toString().replaceAll("-", "");
+	}
+}
